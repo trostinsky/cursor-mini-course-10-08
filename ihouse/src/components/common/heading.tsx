170385efc0092ae998711100f1d0138e12897e2c
Vlad Gribennikov <gribennikov.vlad@gmail.com>
2020-09-03 18:59:20 +0300

    delete all

--- ihouse/src/components/common/heading.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const Heading = styled.h1`
-  font-size: 36px;
-  font-weight: 600;
-`;
--- ihouse/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import {createStore, combineReducers} from "redux";
-import auth from "../reducers/auth";
-
-const store = createStore(combineReducers({
-    auth
-}));
-
-export default store;
